8f593758b3f68dff0b202391a68e60b7b9a2f09b
mendes <ymendeszx@gmail.com>
2026-07-29 18:59:07 -0300

    Update function_listaox.c

--- function_listaox.c
+++ function_listaox.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int validar_caractere(char c) {
-    if (c == 'X') {
+int validar_caractere(char c) 
+{
+    if (c == 'X') 
+    {
         return 1;
     }
-    if (c == 'O') {
+    if (c == 'O') 
+    {
         return 1;
     }
     return 0;
